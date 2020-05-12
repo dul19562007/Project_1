f930eab48f9bb7d6e61281bf89c57c75431c8922
Iluy <dul19562007@yandex.ru>
2020-05-12 12:52:45 +0700

    project part: one

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 'use strict'
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('Какую дадите оценку этому фильму?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('Какую дадите оценку этому фильму?', '');
+
+const perconalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+perconalMovieDB.movies[a] = b;
+perconalMovieDB.movies[c] = d;
+
+console.log(perconalMovieDB);
 
 
 
